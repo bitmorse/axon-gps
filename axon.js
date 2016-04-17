58316f3516b6839352ca9c782103e1bf8dbc32d6
Sam Sulaimanov <sam@bitmorse.com>
2016-04-17 04:34:50 +0200

    fixing bug

--- axon.js
+++ axon.js
@@ -1,19 +1,19 @@
-
-//define map center
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-//which tiles we're using
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
-
-//draw marker
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
 (function(){
 
 	//call the UI page "home"
 	App.load('home');
+
+	//define map center
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+	//which tiles we're using
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(mymap);
+
+	//draw marker
+	var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
 
 	setInterval(function(){
 
