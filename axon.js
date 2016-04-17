2f44f4360d0d81897aa3caa6f780f277d1411d5b
Sam Sulaimanov <sam@bitmorse.com>
2016-04-17 04:31:58 +0200

    fixing var err

--- axon.js
+++ axon.js
@@ -5,7 +5,7 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 //which tiles we're using
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(mymap);
 
 //draw marker
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
