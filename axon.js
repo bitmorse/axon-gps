727e6551fe2b7f932bfc0138e10fb04865c84ec2
Sam Sulaimanov <sam@bitmorse.com>
2016-04-17 04:39:01 +0200

    updating gps

--- axon.js
+++ axon.js
@@ -3,18 +3,21 @@
 	//call the UI page "home"
 	App.load('home');
 
-	//define map center
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	var mymap = L.map('mapid');
 
 	//which tiles we're using
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(mymap);
 
+	//poll gps
 	setInterval(function(){
 
 		$.getJSON( "http://localhost:8080/nervousnet-api/raw-sensor-data/GPS", function( data ) {
-			
+
+				//set map center and zoom level
+				mymap.setView([data.lat, data.long], 19);
+
 				//draw marker
 				var marker = L.marker([data.lat, data.long]).addTo(mymap);
 
